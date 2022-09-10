df2eae6ea7187485b583f73132546ddb37a0a322
Irina-Bondarenko <96590953+Irina-Bondarenko@users.noreply.github.com>
2022-09-10 20:55:24 +0300

    init

---
+++ btn-group/src/App.js
@@ -0,0 +1,12 @@
+import ButtonGroup from "./component/button-group";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonGroup />
+    </div>
+  );
+}
+
+export default App;
